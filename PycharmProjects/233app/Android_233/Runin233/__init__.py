ffb6f80c76afe3e4dcb6e68a3d46abab741d7902
iori422 <iori422@163.com>
2017-12-18 10:46:33 +0800

    优化结构： 新建run路径

---
+++ PycharmProjects/233app/Android_233/Runin233/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : __init__.py.py
+# @Author: tutubuhaoci
+# @Date  : 2017/12/18
+# @Desc  : 
+import os
+import re
+from selenium import webdriver
+from appium import webdriver
+import time
